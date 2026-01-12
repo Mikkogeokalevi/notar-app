99d60b68d92787b4a7b984e338287d8a5e87f128
mikkokalevi <toni@kauppinen.info>
2026-01-12 22:19:07 +0200

    Paivitys ma 12.01.2026 klo 22.19.07,70

--- paivitys.bat
+++ paivitys.bat
@@ -1,16 +1,26 @@
 @echo off
-echo --- Tallennetaan muutokset... ---
-git add .
-git commit -m "Päivitys ja julkaisu"
-
+echo ==========================================
+echo      NOTAR APP - PAIVITYS & LOKI
+echo ==========================================
 echo.
-echo --- Lähetetään koodit GitHubiin... ---
+
+echo 1. Tallennetaan muutokset Gitiin...
+git add .
+git commit -m "Paivitys %date% klo %time%"
 git push
 
 echo.
-echo --- Rakennetaan ja julkaistaan nettisivu... ---
+echo 2. Julkaistaan nettiin (npm run deploy)...
+echo Tama vaihe voi kestaa hetken.
 call npm run deploy
 
 echo.
-echo --- VALMIS! Nyt voit sulkea ikkunan. ---
+echo 3. Kirjataan lokitiedostoon (deploy_log.txt)...
+echo PAIVITYS TEHTY: %date% klo %time% >> deploy_log.txt
+echo ----------------------------------- >> deploy_log.txt
+
+echo.
+echo ==========================================
+echo      VALMIS! LOKI PAIVITETTY.
+echo ==========================================
 pause
