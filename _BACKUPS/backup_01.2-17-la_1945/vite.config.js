cdf0108fc9df43ed697c1899284b6c9f5df69fe1
mikkokalevi <toni@kauppinen.info>
2026-01-17 20:44:12 +0200

    Siivous: Poistettu _BACKUPS ja loki versionhallinnasta

--- _BACKUPS/backup_01.2-17-la_1945/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/notar-app/', // <--- TÄRKEÄ: Pitää olla sama kuin repon nimi!
-})
